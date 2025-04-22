a76df3b3501e714661f1146ccb568af06423ebb3
Kalana-indula <kalanasmedia@outlook.com>
2025-04-22 20:41:50 +0530

    Admin entity created ✔️

---
+++ src/main/java/com/app/medicareapp/MedicareappApplication.java
@@ -0,0 +1,13 @@
+package com.app.medicareapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicareappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicareappApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/app/medicareapp/entity/Admin.java
@@ -0,0 +1,24 @@
+package com.app.medicareapp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+}
